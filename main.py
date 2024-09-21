aa5275793e021bbcf3d3b94b3bf1f4cc909247af
maaya <huki.j37@gmail.com>
2024-09-21 16:29:42 +0900

    英語にへんこう

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import redis
 import streamlit as st
 import os
 
-NO_DESTINATION_MSG = '**行先は未定です**'
+NO_DESTINATION_MSG = '**NO_DESTINATION**'
 HOST_NAME = os.environ['HOST_NAME']
 ACCESS_KEY = os.environ['PASSWORD']
 
@@ -18,19 +18,18 @@ redis_conn = redis.StrictRedis(
 
 # 表示処理
 def main():
-    st.title('[東京版] 指令さんぽゲーム～次の行先はどこ？～')
+    st.title('Destination Designation System')
 
-    st.markdown('現在のデータソース')
+    st.markdown('Using Data below:')
 
     '''
-    下記の条件を満たした駅のみが登録されています。
-    - 東京都内の駅
-    - 駅名に「橋」が入ってる
+    - Train station inside Tokyo
+    - Stations with "橋" in the name
     '''
 
     # 現在の行先を表示
     destination = redis_conn.get('destination')
-    st.markdown('### 現在の行先')
+    st.markdown('### Current Destination')
 
     # 行先表示。行先が未定の場合は未設定メッセージを表示
     next_station = st.empty()
@@ -43,37 +42,37 @@ def main():
 
     # このボタンを押すと行先がゴールとして達成済みセットに登録される
     goal = st.empty()
-    if goal.button('ゴール!'):
+    if goal.button('Arrived!'):
         if destination:
             redis_conn.sadd('achieved', destination)
             redis_conn.delete('destination')
             next_station.markdown(NO_DESTINATION_MSG)
         else:
-            st.warning(NO_DESTINATION_MSG + ' 目的地を設定してください。')
+            st.warning(NO_DESTINATION_MSG + ' Please set destination')
 
     # このボタンを押すと未達成セットからランダムでデータを1つ取得して表示する
     next_destination = st.empty()
-    if next_destination.button('次の行先を決める'):
+    if next_destination.button('Next'):
         if destination is None or destination == b'':
             next: str = redis_conn.spop('unachieved')
             next = next.decode('utf-8')
             redis_conn.set('destination', next)
             next_station.write(next)
         else:
-            st.warning('現在ミッション進行中です' + '先にゴールしてください。')
+            st.warning('You are destinating now.' + 'Please click after arrived.')
 
     # 達成状況を表示
-    st.markdown('### 達成状況')
+    st.markdown('### Completed Status')
 
     unachieved_count = st.empty()
     achieved_count = st.empty()
 
-    if st.button('達成状況の確認・更新'):
+    if st.button('Status Check'):
         unachieved_st_cnt = redis_conn.scard('unachieved')
         achieved_st_cnt = redis_conn.scard('achieved')
 
-        achieved_count.markdown(f'達成済み: {achieved_st_cnt} ')
-        unachieved_count.markdown(f'未達成: {unachieved_st_cnt} ')
+        achieved_count.markdown(f'achieved: {achieved_st_cnt} ')
+        unachieved_count.markdown(f'unachieved: {unachieved_st_cnt} ')
 
 # メイン処理
 if __name__ == '__main__':
